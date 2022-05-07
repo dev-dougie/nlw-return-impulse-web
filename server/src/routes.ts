7084f7812b73f76eb6d975be51639576caa1ffbd
dev-dougie <dougllasp.s@outlook.com>
2022-05-06 22:02:53 -0300

    adding try catch

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,19 +10,25 @@ export const routes = express.Router();
 
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body
-    //wait the operation be finished
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-    const nodemailerMailAdapter = new NodemailerMailAdapter();
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
+
+    try {
+        //wait the operation be finished
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+        const nodemailerMailAdapter = new NodemailerMailAdapter();
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
 
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot
-    });
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot
+        });
 
-    return res.status(201).send()
+        return res.status(201).send()
+    } catch (err) {
+        console.log('error', console.error();
+        return res.status(201).send()
+    }
 });
 //SQLite, doesnt need install any aditional tool
 //PRISMA doest need to use 
